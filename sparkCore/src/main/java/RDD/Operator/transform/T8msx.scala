b9b428dfee7b22129ba62afe200c4ff4c14776cb
IAmMsx <894572503@qq.com>
2021-07-23 19:49:21 +0800

    think 7.22

---
+++ sparkCore/src/main/java/RDD/Operator/transform/T8msx.scala
@@ -0,0 +1,13 @@
+package RDD.Operator.transform
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object T8msx {
+    def main(args: Array[String]): Unit = {
+        val msx = new SparkConf().setMaster("local").setAppName("msx")
+        val context = new SparkContext(msx)
+
+        context.stop()
+    }
+
+}
